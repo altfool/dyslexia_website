a654970ea057ed2391d7a77779131ec7fd19ddc3
Zongming Yang <zongmiy@clemson.edu>
2019-02-13 04:40:48 -0500

    interface modified, import dyslexia website text

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,8 @@ def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html', context)
+    # return render(request, 'blog/home_posts.html', context)
+    return render(request, 'blog/home.html')
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
