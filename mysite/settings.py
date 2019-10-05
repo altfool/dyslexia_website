b2eb0fd23f6364f6906059dd48a8bea5805fa48c
Zack Y <zongmiy@clemson.edu>
2019-10-05 07:56:06 +0000

    deploy configure

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import json
+
+with open('/etc/config-dysleixa.json') as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '49328&m5=t%yartnop1@1o64(@o@32k&z@zz)0c929i_(b4dnc'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = ['130.127.49.191']
 #ALLOWED_HOSTS = []
@@ -134,3 +139,10 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'users-login'
 
 ADMINS = [('dyslexia consortium', 'dyslexia.consortium@gmail.com')]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config.get('EMAIL_USER') 
+EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
